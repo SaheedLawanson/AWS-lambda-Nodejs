0924fcd8423a0bb1ccc4b6b69c769ff4ec9309f0
Saheed Lawanson <saheedlawanson47@yahoo.com>
2021-12-31 19:27:40 +0100

    commenting

--- AWS-backend-nodejs.js
+++ AWS-backend-nodejs.js
@@ -1,19 +1,27 @@
+// Get AWS development kit
 const AWS = require('aws-sdk')
+// Create an instance of the dynamodb object
 const docClient = new AWS.DynamoDB.DocumentClient()
-
+// table to be queried
 const tableName = "Colors"
 
+// Handler function
 exports.handler = (event, context, callback) => {
     
+    // Define the parameters to obtain an items from dynamodb
+    // tables
     let params = {
+        // Name of the table to be queried
         TableName: tableName,
         Key: {
+            // The object ID to be obtained
             "ID": "01"
         }
     }
     
+    // Finally return the color with that ID in the table
     docClient.get(params, (err, data) => {
-        callback(err, data)
+        callback(err, data.Item.Color)
     })
     
 };
